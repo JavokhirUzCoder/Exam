a936536d36adc1fb32c8f84866a897159d1522cd
JavokhirUzCoder <131436276+JavokhirUzCoder@users.noreply.github.com>
2023-04-21 18:57:22 +0500

    Update #03.py

--- python exam#02/#03.py
+++ python exam#02/#03.py
@@ -1,10 +1,15 @@
 email = input("Email kiritib ko`ringchi:  ")
 
 trueEmailSymbol = "@"
+trueEmailMust = ".com"
 
-trueEmailSymbol2 = ".com"
+trueEmailSymbolReal = True
+DoNotUse = ["!", "#", "$", "%", "^", "&", "*", "(", ")", "+","-","/",","]
+for i in DoNotUse:
+    if i in email:
+        trueEmailSymbolReal = False
 
-if email == email.lower() and trueEmailSymbol in email and trueEmailSymbol2 in email:
+if email == email.lower() and (trueEmailSymbol in email) and (trueEmailMust in email) and trueEmailSymbolReal:
     print("Yes, This email is valid")
 else:
     print("No, This email is invalid")
