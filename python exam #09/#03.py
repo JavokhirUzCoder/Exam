8bb53c16bb8ce71096505fcf74598deebc066979
JavokhirUzCoder <131436276+JavokhirUzCoder@users.noreply.github.com>
2023-05-15 18:09:29 +0500

    Add files via upload

---
+++ python exam #09/#03.py
@@ -0,0 +1,5 @@
+result = [i for i in range(1,21)]
+
+result = list(filter(lambda x: x%2==0,result))
+
+print(result)
---
+++ python exam #09/#04.py
@@ -0,0 +1,3 @@
+result = [i for i in range(1,21)]
+result = list(map(lambda x: x**2, result))
+print(result)
